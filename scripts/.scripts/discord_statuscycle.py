914828d7dcce82ab4f5dc72bc37a5f5581877936
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2021-01-16 15:10:06 +0100

    Sat Jan 16 03:10:06 PM CET 2021

---
+++ scripts/.scripts/discord_statuscycle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import itertools
+import requests as req
+from time import sleep
+
+# TODO: get data from CLI args, including the token and the actual status and emojis
+
+ACC_TOKEN = "mfa.iVCbhzhl1fSVaiWd0T7XsGmYHO0YbOOuIAJloG_QZjWi2jweb75ZERZMKcgBYzIdLogEbT_GyRDTxjVilEQF"
+STATUS_URL = "https://discordapp.com/api/v6/users/@me/settings"  
+
+DELAY_SEC = 1
+
+DATA = [(None, '👍🏿'), (None, '✌🏿'), (None, '👎🏿')]
+
+req_headers = {
+    "authorization": ACC_TOKEN,
+    "Content-Type": "application/json"
+}
+
+once = False
+
+for combo in itertools.cycle(DATA):
+    result = req.patch(STATUS_URL, headers=req_headers, json={
+        "custom_status": {
+            "text": combo[0] or "",
+            "emoji_name": combo[1] or ""
+        }
+    })
+
+    if not once:
+        print(result.request.headers)
+        once = True
+
+    sleep(DELAY_SEC)
